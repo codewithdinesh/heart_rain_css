3c6080121930bb37c8e12c25908628868f5e9900
Dinesh Rathod <72983957+codewithdinesh@users.noreply.github.com>
2021-10-03 10:01:20 +0530

    Adding Stylesheet ,index html file and script FIle

---
+++ script.js
@@ -0,0 +1,12 @@
+function Rain() {
+    const heart = document.createElement('div');
+    heart.innerText = "💜";
+    heart.style.right = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 4 + "s";
+    heart.classList.add("heart");
+    setTimeout(()=>{
+        heart.remove();
+    },5000)
+    document.body.appendChild(heart);
+}
+setInterval(Rain, 110);
